ccae36d462f92e60db0ba44d16eff435a9be4889
jlipkin8 <jlipkin8@gmail.com>
2017-09-20 23:47:46 +0000

    striped all the white space for the line, not just the white space on the right side

--- wordcount.py
+++ wordcount.py
@@ -1,15 +1,25 @@
 # put your code here.
-poem = open("twain.txt")
-word_counts = {}
+# poem = open("twain.txt")
+# word_counts = {}
 
-for line in poem:
-    line = line.rstrip()
-    words = line.split(" ")
+# for line in poem:
+#     line = line.rstrip()
+#     words = line.split(" ")
 
-    for word in words:
-        word_counts[word] = word_counts.get(word, 0) + 1
+#     for word in words:
+#         word_counts[word] = word_counts.get(word, 0) + 1
 
-for word, count in word_counts.items():
-    print "{} {}".format(word, count)
+# for word, count in word_counts.items():
+#     print "{} {}".format(word, count)
 
-poem.close()
+# poem.close()
+
+
+text = open("twain.txt")
+word_count = {}
+
+for line in text:
+    line = line.strip()
+    if line:
+        words = line.split(" ")
+        print words
