6a74c2d4b4913ea420ee74226f7580ff1e9c2288
jlipkin8 <jlipkin8@gmail.com>
2017-10-04 20:04:04 +0000

    removed count_words because of issues accesing the dictionary from other functions

--- wordcount.py
+++ wordcount.py
@@ -20,12 +20,7 @@ def rmv_end(m):
     return word 
 
 
-def count_word(wrd):
-    word = wrd.lower()
-    word_count[word] = word_count.get(word, 0) + 1
-
-
-def iter_inspect_wrds(words):
+def iter_inspect_wrds(words, word_count):
     for i, word in enumerate(words):
         if re.search("^\"$", word):
             del words[i]
@@ -39,7 +34,25 @@ def iter_inspect_wrds(words):
         elif re.search("\w+\"$", word): 
             word = re.sub("\w+\"$", rmv_end, word)
 
-        count_word(word) 
+        word = word.lower()
+        word_count[word] = word_count.get(word, 0) + 1
+
+
+def count_words_dict(text):
+    word_count = {}
+    count = 0
+    for line in text:
+        line = line.strip()
+        if line:
+            words = line.split(" ")
+            iter_inspect_wrds(words, word_count)
+
+    return word_count
+
+
+def print_word_count(word_dict): 
+    for word, count in word_dict.items():
+        print "{} {}".format(word, count)
 
 
 filename = sys.argv[1]
@@ -47,16 +60,7 @@ filename = sys.argv[1]
 if filename:
     text = open(filename)
 
-word_count = {}
-count = 0
+words = count_words_dict(text)
+print_word_count(words)
 
-for line in text:
-    line = line.strip()
-    if line:
-        words = line.split(" ")
-        iter_inspect_wrds(words)
-
-
-# for word, count in word_count.items():
-#     print "{} {}".format(word, count)
-# text.close()
+text.close()
