4d521b1e24c20afdce55693c10eef66c143f6bf1
jlipkin8 <jlipkin8@gmail.com>
2017-10-04 19:16:46 +0000

    defined the functions count_word and iter_inspect_wrds to make the script more readable

--- wordcount.py
+++ wordcount.py
@@ -1,18 +1,4 @@
-# put your code here.
-# poem = open("twain.txt")
-# word_counts = {}
 
-# for line in poem:
-#     line = line.rstrip()
-#     words = line.split(" ")
-
-#     for word in words:
-#         word_counts[word] = word_counts.get(word, 0) + 1
-
-# for word, count in word_counts.items():
-#     print "{} {}".format(word, count)
-
-# poem.close()
 import re
 
 def rmv_front_char(m):
@@ -20,20 +6,40 @@ def rmv_front_char(m):
     word = word[1:]       
     return word
 
+
 def rmv_back_2_char(m): 
     word = m.group()
     word = word[:-2]
     return word
 
+
 def rmv_end(m):
-    print "rmv_end " 
     word = m.group()
     word = word[:-1]
     return word 
 
-def check_word(wrd):
+
+def count_word(wrd):
     word = wrd.lower()
-    word_count[word] = word_count.get(word, 0) + 1 
+    word_count[word] = word_count.get(word, 0) + 1
+
+
+def iter_inspect_wrds(words):
+    for i, word in enumerate(words):
+        if re.search("^\"$", word):
+            del words[i]
+            continue 
+        elif re.search("^\"\w*", word):
+            word = re.sub("^\"\w*", rmv_front_char, word)
+        elif re.search("\w+[\.\,\?\)\;\:\!]$", word):
+            word = re.sub("\w+[\.\,\?\)\;\:\!]$", rmv_end, word)
+        elif re.search("\w+[\.\,\?\)\;\:\!]\"$", word): 
+            word = re.sub("\w+[\.\,\?\)\;\:\!]\"$",rmv_back_2_char, word)
+        elif re.search("\w+\"$", word): 
+            word = re.sub("\w+\"$", rmv_end, word)
+
+        count_word(word) 
+
 
 text = open("twain.txt")
 word_count = {}
@@ -43,27 +49,11 @@ for line in text:
     line = line.strip()
     if line:
         words = line.split(" ")
-        for i, word in enumerate(words):
-            if re.search("^\"$", word):
-                del words[i]
-                continue 
-            elif re.search("^\"\w*", word):
-                word = re.sub("^\"\w*", rmv_front_char, word)
-            elif re.search("\w+[\.\,\?\)\;\:\!]$", word):
-                word = re.sub("\w+[\.\,\?\)\;\:\!]$", rmv_end, word)
-            elif re.search("\w+[\.\,\?\)\;\:\!]\"$", word): 
-                word = re.sub("\w+[\.\,\?\)\;\:\!]\"$",rmv_back_2_char, word)
-            elif re.search("\w+\"$", word): 
-                word = re.sub("\w+\"$", rmv_end, word)
-            else: 
-                print word
-
-            word = word.lower()
-            word_count[word] = word_count.get(word, 0) + 1 
+        iter_inspect_wrds(words)
 
 
-# for word, count in word_count.items():
-#     print "{} {}".format(word, count)
+for word, count in word_count.items():
+    print "{} {}".format(word, count)
 text.close()
 
 
